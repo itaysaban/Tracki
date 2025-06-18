b1ca472703aa489b7cd18dbc4b8a21e9dd60e353
itaysaban <itaysaban21@gmail.com>
2025-06-18 19:17:35 +0300

    added about screen

---
+++ Tracki/app/about.tsx
@@ -0,0 +1,23 @@
+import { Background } from "@react-navigation/elements";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Expo.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  
+  text: {
+    color: "white"
+  }
+})
