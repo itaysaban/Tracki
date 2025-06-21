ea48bddebe2a5f899e2af9042486ecaa53235554
itaysaban <itaysaban21@gmail.com>
2025-06-21 11:36:43 +0300

    added open and close cases display to the home page

---
+++ Tracki/dummy_data/dummy_data.tsx
@@ -0,0 +1,15 @@
+
+
+const openCases = [
+  { id: '1', name: 'Sarah Cohen', lastSeen: 'Tel Aviv', image: require('../assets/images/missing profiles/kid 3.jpeg')},
+  { id: '2', name: 'David Levi', lastSeen: 'Haifa', image: require('../assets/images/missing profiles/Kid 1.jpeg')},
+  { id: '3', name: 'Nora Barak', lastSeen: 'Jerusalem', image: require('../assets/images/missing profiles/kid 2.jpeg') },
+];
+
+
+const closedCases = [
+ { id: '1', name: 'Itay Saban', lastSeen: 'Tel Aviv' ,image: require('../assets/images/missing profiles/kid 4.png')}
+]
+
+
+export default {openCases, closedCases};
